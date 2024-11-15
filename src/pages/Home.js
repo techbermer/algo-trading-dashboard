eaccaa07a6477618080a2a82651dcea3d9a10389
PRAMOD-SAMSON <pramod@bermer.in>
2024-11-15 16:19:14 +0530

    Backend Apis integration

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import { logout } from "../apis/authApis";
+import { startMarket, stopMarket } from "../apis/marketDataApis";
 import HomeDecorImg from "../assets/images/HomeDecorativeImg.png";
 import BlackBackgroundImg from "../assets/images/HomeBackgroundImg.png";
 import { MARKET_OPTIONS } from "../constants/markets";
@@ -7,10 +9,17 @@ import "../stylings/Home.css";
 
 const Home = ({ onLogout }) => {
   const navigate = useNavigate();
-  const dropdownRef = useRef(null);
+  const marketDropdownRef = useRef(null);
+  const lotSizeDropdownRef = useRef(null);
   const [token, setToken] = useState(null);
+  const [toastType, setToastType] = useState("error");
+  const [toastMessage, setToastMessage] = useState("");
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [selectedMarket, setSelectedMarket] = useState("Select Market");
+  const [selectedMarketInstrumentKey, setSelectedMarketInstrumentKey] =
+    useState("");
+  const [isLotSizeDropdownOpen, setIsLotSizeDropdownOpen] = useState(false);
+  const [selectedLotSize, setSelectedLotSize] = useState("Select Lot Size");
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -21,8 +30,14 @@ const Home = ({ onLogout }) => {
 
   useEffect(() => {
     const handleClickOutside = (event) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      if (
+        marketDropdownRef.current &&
+        !marketDropdownRef.current.contains(event.target) &&
+        lotSizeDropdownRef.current &&
+        !lotSizeDropdownRef.current.contains(event.target)
+      ) {
         setIsDropdownOpen(false);
+        setIsLotSizeDropdownOpen(false);
       }
     };
 
@@ -30,20 +45,132 @@ const Home = ({ onLogout }) => {
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, [dropdownRef]);
+  }, []);
 
   const handleMarketSelect = (option) => {
     setSelectedMarket(option.label);
+    setSelectedMarketInstrumentKey(option.value);
     setIsDropdownOpen(false);
-    navigate("/market", { state: { instrumentKey: option.value, token } });
   };
 
-  const handleLogout = () => {
-    onLogout();
+  const handleLotSizeSelect = (size) => {
+    setSelectedLotSize(size);
+    setIsLotSizeDropdownOpen(false);
   };
+
+  const handleLogout = async () => {
+    try {
+      const response = await logout();
+      if (response.ok) {
+        setToastMessage("Market stopped");
+        setToastType("error");
+        onLogout();
+      } else {
+        console.error("Failed to logout");
+      }
+    } catch (error) {
+      console.error("Error during logout:", error);
+    }
+  };
+
+  const start_Market = async () => {
+    if (
+      !selectedMarketInstrumentKey ||
+      selectedMarketInstrumentKey === "Select Market"
+    ) {
+      setToastMessage("Please select a market to start.");
+      setToastType("error");
+      return;
+    }
+
+    if (!selectedLotSize || selectedLotSize === "Select Lot Size") {
+      setToastMessage("Please select a lot size to start.");
+      setToastType("error");
+      return;
+    }
+
+    const lotData = {
+      NIFTY_50:
+        selectedMarketInstrumentKey === "NSE_INDEX|Nifty 50"
+          ? selectedLotSize
+          : 0,
+      BANK_NIFTY:
+        selectedMarketInstrumentKey === "NSE_INDEX|Bank Nifty"
+          ? selectedLotSize
+          : 0,
+      FIN_NIFTY:
+        selectedMarketInstrumentKey === "NSE_INDEX|Fin Nifty"
+          ? selectedLotSize
+          : 0,
+      MIDCPNIFTY:
+        selectedMarketInstrumentKey === "NSE_INDEX|Midcap Nifty"
+          ? selectedLotSize
+          : 0,
+      SENSEX:
+        selectedMarketInstrumentKey === "BSE_INDEX|Sensex"
+          ? selectedLotSize
+          : 0,
+      BANKEX:
+        selectedMarketInstrumentKey === "BSE_INDEX|Bankex"
+          ? selectedLotSize
+          : 0,
+    };
+
+    const body = JSON.stringify({
+      instrument_key: selectedMarketInstrumentKey,
+      lot: lotData,
+    });
+
+    try {
+      const response = await startMarket({ selectedMarketAndLot: body });
+
+      if (response.ok) {
+        navigate("/market", {
+          state: { instrumentKey: selectedMarketInstrumentKey, token },
+        });
+      } else {
+        console.error("Failed to start market");
+      }
+    } catch (error) {
+      console.error("Error while starting market:", error);
+    }
+  };
+
+  const stop_Market = async () => {
+    try {
+      const response = await stopMarket();
+
+      if (response.ok) {
+        setToastMessage("Market operations stopped");
+        setToastType("error");
+      } else {
+        console.error("Failed to stop market");
+      }
+    } catch (error) {
+      console.error("Error while stopping market:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (toastMessage) {
+      const timer = setTimeout(() => {
+        setToastMessage("");
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [toastMessage]);
 
   return (
     <div className="home-wrapper">
+      {toastMessage && (
+        <div className={`custom-toast ${toastType}`}>
+          <span>{toastMessage}</span>
+          <button className="close-btn" onClick={() => setToastMessage("")}>
+            ×
+          </button>
+        </div>
+      )}
       <h2 className="home-title">
         Trade <span>X</span>
       </h2>
@@ -59,27 +186,60 @@ const Home = ({ onLogout }) => {
             Navigate and manage your trading strategies effortlessly with an
             intuitive and easy-to-use platform.
           </h6>
-          <div className="dropdown-container" ref={dropdownRef}>
-            <button
-              className="dropdown-toggle"
-              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-            >
-              {selectedMarket} ▼
-            </button>
-            {isDropdownOpen && (
-              <ul className="dropdown-menu">
-                {MARKET_OPTIONS.map((option) => (
-                  <li
-                    key={option.value}
-                    onClick={() => handleMarketSelect(option)}
-                  >
-                    {option.label}
-                  </li>
-                ))}
-              </ul>
-            )}
+
+          <div className="home-buttons">
+            <div className="dropdown-container" ref={marketDropdownRef}>
+              <button
+                className="dropdown-toggle"
+                onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+              >
+                {selectedMarket} ▼
+              </button>
+              {isDropdownOpen && (
+                <ul className="dropdown-menu">
+                  {MARKET_OPTIONS.map((option) => (
+                    <li
+                      key={option.value}
+                      onClick={() => handleMarketSelect(option)}
+                    >
+                      {option.label}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+            <div className="dropdown-container" ref={lotSizeDropdownRef}>
+              <button
+                className="dropdown-toggle"
+                onClick={() => setIsLotSizeDropdownOpen(!isLotSizeDropdownOpen)}
+              >
+                {selectedLotSize} ▼
+              </button>
+              {isLotSizeDropdownOpen && (
+                <ul className="dropdown-menu">
+                  {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((size) => (
+                    <li key={size} onClick={() => handleLotSizeSelect(size)}>
+                      {size}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+
+            <div className="dropdown-container">
+              <button className="dropdown-toggle" onClick={start_Market}>
+                Start
+              </button>
+            </div>
+
+            <div className="dropdown-container">
+              <button className="dropdown-toggle" onClick={stop_Market}>
+                Stop
+              </button>
+            </div>
           </div>
         </div>
+
         <div className="home-content-right">
           <img className="home-decor-img" src={HomeDecorImg} />
         </div>
